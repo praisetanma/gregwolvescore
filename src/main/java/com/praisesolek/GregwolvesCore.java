2030ce35ebdd3f9ea63911b5271e3977a82c5ca3
praisesolek <praisesolek@ninjasheepgames.com>
2025-10-18 14:24:52 -0400

    Set up Architectury Loom

---
+++ src/main/java/com/praisesolek/GregwolvesCore.java
@@ -0,0 +1,8 @@
+package com.praisesolek;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(GregwolvesCore.MODID)
+public class GregwolvesCore {
+    public static final String MODID = "gregwolvescore";
+}
